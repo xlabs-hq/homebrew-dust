c7c0f0f37ce3035fdf37b336c91eba261ce058e7
James Tippett <james.tippett@gmail.com>
2026-04-14 22:44:56 +0700

    Update formula for dust

--- Formula/dust.rb
+++ Formula/dust.rb
@@ -1,18 +1,32 @@
 class Dust < Formula
   desc "An open-source, DB-backed, reactive map for when you want to share something reliably between applications"
   homepage "https://dustlayer.io"
-  url "https://github.com/xlabs-hq/dust/archive/refs/tags/cli-v0.2.0.tar.gz"
-  sha256 "ffec9edb021f866e799b0a992a767a4036a1b0820cf08be9a0871466465158aa"
-  license "MIT"
   version "0.2.0"
+  license "MIT"
 
-  depends_on "crystal" => :build
+  on_macos do
+    on_arm do
+      url "https://github.com/xlabs-hq/dust/releases/download/cli-v0.2.0/dust-cli-0.2.0-aarch64-apple-darwin.tar.gz"
+      sha256 "093129e392600770c53e238eabb597e6689ed965e920a31f147f63e3b15aa537"
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/xlabs-hq/dust/releases/download/cli-v0.2.0/dust-cli-0.2.0-aarch64-unknown-linux-musl.tar.gz"
+      sha256 "dff69ca6b79b7b45367e0967530f65a096377caedc6861e80c6b6a196d6fb3eb"
+    end
+
+    on_intel do
+      url "https://github.com/xlabs-hq/dust/releases/download/cli-v0.2.0/dust-cli-0.2.0-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "269bb7b4837801591d6ff58fb6c5c057b060fb90861a84ab9db4d2894f4bb3a2"
+    end
+  end
 
   def install
-    cd "cli" do
-      system "shards", "build", "--release", "--no-debug", "--production"
-      bin.install "bin/dust"
-    end
+    binary = buildpath.glob("**/dust").reject(&:directory?).first
+    raise "binary `dust` not found in extracted release asset" if binary.nil?
+    bin.install binary => "dust"
   end
 
   test do
