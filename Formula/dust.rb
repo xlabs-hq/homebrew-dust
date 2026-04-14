e77a90e22272bb5e855cd668af0e87e782692d0f
James Tippett <james.tippett@gmail.com>
2026-04-14 21:51:19 +0700

    Update formula for dust

--- Formula/dust.rb
+++ Formula/dust.rb
@@ -2,7 +2,7 @@ class Dust < Formula
   desc "An open-source, DB-backed, reactive map for when you want to share something reliably between applications"
   homepage "https://dustlayer.io"
   url "https://github.com/xlabs-hq/dust/archive/refs/tags/cli-v0.2.0.tar.gz"
-  sha256 "dca087f920a688b33e29320a5f642edd4820ac76393fd1b6e4b5ad1f2ca08f3d"
+  sha256 "ffec9edb021f866e799b0a992a767a4036a1b0820cf08be9a0871466465158aa"
   license "MIT"
   version "0.2.0"
 
